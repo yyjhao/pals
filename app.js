07c6bea84a5fe648c0bf1291dac2d54ed1cc4024
yyjhao <yyjhao@gmail.com>
2014-03-08 16:25:30 +0800

    login routine

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function() {
-    app.set('port',config.port);
+    app.set('port', config.port);
     app.use(express.favicon());
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
@@ -20,8 +20,6 @@ app.configure(function() {
         appId: config.fb.id,
         secret: config.fb.secret
     }));
-    app.use(express.methodOverride());
-    app.use(app.router);
 });
 
 app.configure('production', function() {
@@ -34,47 +32,45 @@ app.configure('development', function() {
 });
 
 app.get('/fb_login', Facebook.loginRequired(), function(req, res) {
-        res.redirect('/');
+    res.redirect('/');
 });
 
 app.get('/fb', function(req, res) {
-    // if (eh) {
-    //     return res.send(eh);
-    // }
-    // req.facebook.api('/me/friends?fields=mutualfriends,name', function(err, data) {
-    //     if (err) {
-    //         console.log('err', err);
-    //         return res.status(500);
-    //     } else if (data.error_code) {
-    //         console.log('err', data);
-    //     } else {
-    //         var graph = Graph.fromFB(data.data);
-    //         graph.computeCommunities(function(err, nc, pos) {
-    //             if (err) {
-    //                 console.log(err);
-    //                 return res.status(500);
-    //             }
-    //             res.send({
-    //                 nodes: graph.nodes,
-    //                 edges: graph.edges,
-    //                 communities: nc,
-    //                 positions: pos
-    //             });
-    //         });
-    //     }
-    // });
-    var graph = Graph.fromFB(require('./graph'));
-    graph.computeCommunities(function(err, nc, pos) {
+    req.facebook.api('/me/friends?fields=mutualfriends,name', function(err, data) {
         if (err) {
-            return console.log(err);
+            console.log('err', err);
+            return res.send(500);
+        } else if (data.error_code) {
+            console.log('fb err', data);
+            res.send(data);
+        } else {
+            var graph = Graph.fromFB(data.data);
+            graph.computeCommunities(function(err, nc, pos) {
+                if (err) {
+                    console.log(err);
+                    return res.send(500);
+                }
+                res.send({
+                    nodes: graph.nodes,
+                    edges: graph.edges,
+                    communities: nc,
+                    positions: pos
+                });
+            });
         }
-        res.send({
-            nodes: graph.nodes,
-            edges: graph.edges,
-            communities: nc,
-            positions: pos
-        });
     });
+    // var graph = Graph.fromFB(require('./graph'));
+    // graph.computeCommunities(function(err, nc, pos) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     res.send({
+    //         nodes: graph.nodes,
+    //         edges: graph.edges,
+    //         communities: nc,
+    //         positions: pos
+    //     });
+    // });
 });
 
 
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
-$.get("/fb").done(function(json) {
-    window.g = new GraphRenderer($("#graph-viewer"), json);
+$('.start-button').click(function() {
+    localStorage.setItem('last-try', 0);
+    $("#loading-screen").show();
+    $.get('/fb').done(function(json) {
+        $("#loading-screen").hide();
+        $('#graph-viewer').height(window.innerHeight);
+        $('html, body').animate({
+            scrollTop: $('#graph-viewer').offset().top
+        }, 1000);
+        window.g = new GraphRenderer($('#graph-viewer'), json);
+    }).fail(function() {
+        localStorage.setItem('last-try', Date.now());
+        location = '/fb_login';
+    });
 });
+
+if (Date.now() - parseInt(localStorage.getItem('last-try'), 10) < 300000) {
+    $('.start-button').click();
+}
